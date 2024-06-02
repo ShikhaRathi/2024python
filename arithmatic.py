5c810728bcf83e65a4a6760d350d0c4fd1a5917a
ShikhaRathi <shikha.chandak@gmail.com>
2024-06-02 16:34:03 +0530

    Added code for addition

---
+++ arithmatic.py
@@ -0,0 +1,9 @@
+def Add(num1,num2):
+    add=num1+num2
+    return(add)
+
+x= int(input("Enter the num1:"))
+y =int(input("Enter the num2:"))
+ans = Add(x,y)
+print(f"Sum of {x} and {y} is {ans}")
+
