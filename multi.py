9bc9ed0f960015079d0e45f8cbd044876746a3f6
ShikhaRathi <shikha.chandak@gmail.com>
2024-06-05 10:11:09 +0530

    code for multiplication

--- multi.py
+++ multi.py
@@ -1,8 +1,9 @@
-def mul(num1,num2):
-    pro=num1*num2
+def mul(num1, num2):
+    pro = num1*num2
     return pro
 
-x=10
-y=20
-ans=mul(x,y)
-print(f"muliplication of {x}and {y}is {ans}")
+
+x = 10
+y = 20
+ans = mul(x, y)
+print(f"multiplication of {x}and {y}is {ans}")
